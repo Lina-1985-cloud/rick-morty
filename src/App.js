1ca9a79584d5e73b45a30d8c35b06db660db2569
Lina Marcela <linavilla.2@gmail.com>
2022-07-18 11:16:39 -0500

    Se crea aplicaciÃon para ver informacion de los personajes de rick and morty con React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import imageRickMorty from './img/rick-morty.png'
 import './App.css';
+import { useState } from 'react';
+import Character from './components/Character';
 
 function App() {
+  // Generamos un estado para poder pasarle toda la info a un nuevo componente que sera el encargado de renderizara todos los datos
+  const [characters, setCharacters] = useState(null)
+
+  // Hacemos una petición asincrona de la url de rick and morty para obtener los datos
+  const reqApi = async ()=>{
+    const api = await fetch('https://rickandmortyapi.com/api/character')
+    const characterApi = await api.json();// Nos devuelve un array de objetos con mucha mas informacion para ser mapeada y extraer lo que nececitamos
+    setCharacters(characterApi.results);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='title'>Rick &Morty</h1>
+        {characters ?(
+          <Character characters = {characters} setCharacters={setCharacters}/>
+
+        ) : (
+          <>
+            <img src={imageRickMorty} alt='Rick & Morty' className='img-home'></img>
+            <button onClick={reqApi} className="btn-search">
+              Buscar personajes
+            </button>
+          </>
+        )}
       </header>
     </div>
   );
